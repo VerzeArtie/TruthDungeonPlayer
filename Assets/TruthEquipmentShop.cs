e2d0f59c1cfed20a67ad68a8a84ed97a42e7b134
verze artie <verze.artie@gmail.com>
2015-12-11 19:32:22 +0900

    Create TruthEquipmentShop.cs

---
+++ Assets/TruthEquipmentShop.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+namespace DungeonPlayer
+{
+    public partial class TruthEquipmentShop : MonoBehaviour
+    {
+    }
+}
