6ba27bc8c68dc62d7429e79e2d86da1fe8642aca
verze artie <verze.artie@gmail.com>
2015-12-11 18:26:03 +0900

    Update SceneDimension.cs

--- Assets/SceneDimension.cs
+++ Assets/SceneDimension.cs
@@ -5,4 +5,11 @@ using System.Collections;
 public static class SceneDimension
 {
     public static List<string> playbackScene = new List<string>();
+    
+    public static void Back()
+    {
+        string backScene = playbackScene[0];
+        playbackScene.Clear();
+        Application.LoadLevel(backScene);        
+    }
 }
