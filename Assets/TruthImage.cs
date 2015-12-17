4f0111f58c23176feddc543c943c7369e0d3d832
verze.artie <verze.artie@gmail.com>
2015-12-18 00:51:29 +0900

    constructing countdown number...

--- Assets/TruthImage.cs
+++ Assets/TruthImage.cs
@@ -3,134 +3,159 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class TruthImage : Image
+namespace DungeonPlayer
 {
-    public enum buffType
+    public class TruthImage : Image
     {
-        None,
-        Small,
-        Large,
-    }
-    public enum CumulativeTextAlign
-    {
-        TopRight,
-        Center,
-    }
-
-    protected buffType buffMode = buffType.None;
-    public buffType BuffMode
-    {
-        get { return buffMode; }
-        set
+        public enum buffType
         {
-            buffMode = value;
-            if (value == buffType.Large)
+            None,
+            Small,
+            Large,
+        }
+        public enum CumulativeTextAlign
+        {
+            TopRight,
+            Center,
+        }
+
+        protected buffType buffMode = buffType.None;
+        public buffType BuffMode
+        {
+            get { return buffMode; }
+            set
             {
-//                this.fixedRect = new Rectangle(0, 0, 50, 50);
-//                this.fixedPointCount = new PointF(4, 47);
+                buffMode = value;
+                if (value == buffType.Large)
+                {
+                    //                this.fixedRect = new Rectangle(0, 0, 50, 50);
+                    //                this.fixedPointCount = new PointF(4, 47);
+                }
             }
         }
-    }
-    
-    protected string imageName = string.Empty;
-    public string ImageName
-    {
-        get { return imageName; }
-        set { imageName = value; }
-    }
 
-    protected int count = 0;
-    public int Count
-    {
-        get { return count; }
-        set { count = value; }
-    }
+        protected string imageName = string.Empty;
+        public string ImageName
+        {
+            get { return imageName; }
+            set { imageName = value; }
+        }
 
-    protected int cumulative = 0;
-    public int Cumulative
-    {
-        get { return cumulative; }
-        set { if (value >= 0) { cumulative = value; } }
-    }
-    protected CumulativeTextAlign align = CumulativeTextAlign.TopRight;
-    public CumulativeTextAlign CumulativeAlign
-    {
-        get { return align; }
-        set { align = value; }
-    }
+        protected int count = 0;
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
 
-    public void OnMouseEnterImage()
-    {
+        protected int cumulative = 0;
+        public int Cumulative
+        {
+            get { return cumulative; }
+            set { if (value >= 0) { cumulative = value; } }
+        }
+        protected CumulativeTextAlign align = CumulativeTextAlign.TopRight;
+        public CumulativeTextAlign CumulativeAlign
+        {
+            get { return align; }
+            set { align = value; }
+        }
 
+        public void OnMouseEnterImage()
+        {
+
+        }
+
+        private Text txtCounter;
+        void Start()
+        {
+            GameObject baseObj = new GameObject("object");
+
+            txtCounter = baseObj.AddComponent<Text>();
+
+            txtCounter.name = "counter";
+            //counter.BuffMode = TruthImage.buffType.Small;
+            txtCounter.rectTransform.anchorMin = new Vector2(0.0f, 0.0f);
+            txtCounter.rectTransform.anchorMax = new Vector2(1.0f, 1.0f);
+            txtCounter.rectTransform.pivot = new Vector2(0.5f, 0.5f);
+            //counter.rectTransform.localPosition = new Vector2(0, 0);
+            //counter.rectTransform.position = new Vector2(0, 0);
+            txtCounter.alignment = TextAnchor.UpperCenter;
+            txtCounter.font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+            txtCounter.color = Color.blue;
+            txtCounter.text = this.count.ToString();
+            txtCounter.gameObject.SetActive(true);
+            txtCounter.transform.SetParent(this.transform, false);
+            txtCounter.rectTransform.anchoredPosition = new Vector2(0, -30);
+        }
+        void Update()
+        {
+            txtCounter.text = this.count.ToString();
+        }
+        // todo
+        //    Rectangle fixedRect = new Rectangle(0, 0, 25, 25);
+        //    PointF fixedPointCount = new PointF(4, 22);
+        //    PointF fixedPointCumulative = new PointF(12, -3);
+        //    PointF fixedPointCumulative_2 = new PointF(4, -3);
+        //    PointF fixedPointCumulativeCenter = new PointF(6, 4);
+        //    Font fixedFontCount = new Font("Arial", 11, FontStyle.Bold, GraphicsUnit.Point, (byte)(128));
+        //    Font fixedFontCumulative = new Font("Times New Roman", 14, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, (byte)(128));
+        //    protected override void OnPaint(System.Windows.Forms.PaintEventArgs pe)
+        //    {
+        //        if (buffMode == buffType.None)
+        //        {
+        //            base.OnPaint(pe);
+        //        }
+        //        else
+        //        {
+        //            System.Drawing.Graphics e = pe.Graphics;
+        //            if (this.Image != null)
+        //            {
+        //                e.DrawImage(this.Image, this.fixedRect);
+        //            }
+        //
+        //            if (0 < count && count <= 99)
+        //            {
+        //                e.DrawString(count.ToString(), fixedFontCount, Brushes.DarkGreen, fixedPointCount);
+        //            }
+        //
+        //            if (cumulative > 0)
+        //            {
+        //                if (this.align == CumulativeTextAlign.TopRight)
+        //                {
+        //
+        //                    if (cumulative >= 10)
+        //                    {
+        //                        e.DrawString(cumulative.ToString(), fixedFontCumulative, Brushes.Black, fixedPointCumulative_2);
+        //                    }
+        //                    else
+        //                    {
+        //                        e.DrawString(cumulative.ToString(), fixedFontCumulative, Brushes.Black, fixedPointCumulative);
+        //                    }
+        //                }
+        //                else
+        //                {
+        //                    e.DrawString(cumulative.ToString(), fixedFontCumulative, Brushes.Black, fixedPointCumulativeCenter);
+        //                }
+        //
+        //            }
+        //        }
+        //    }
+
+        public bool AbstractCountDownBuff()
+        {
+            if (this.Count > 0)
+            {
+                this.Count--;
+                //this.Invalidate();
+                if (this.Count <= 0)
+                {
+                    this.sprite = null;
+                    this.Update();
+                    return true; // BUFF効果が切れた事を通達する。
+                }
+            }
+            return false;
+        }
     }
-
-    void Update()
-    {
-        Debug.Log("TruthImage:Update");
-    }
-//    Rectangle fixedRect = new Rectangle(0, 0, 25, 25);
-//    PointF fixedPointCount = new PointF(4, 22);
-//    PointF fixedPointCumulative = new PointF(12, -3);
-//    PointF fixedPointCumulative_2 = new PointF(4, -3);
-//    PointF fixedPointCumulativeCenter = new PointF(6, 4);
-//    Font fixedFontCount = new Font("Arial", 11, FontStyle.Bold, GraphicsUnit.Point, (byte)(128));
-//    Font fixedFontCumulative = new Font("Times New Roman", 14, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, (byte)(128));
-//    protected override void OnPaint(System.Windows.Forms.PaintEventArgs pe)
-//    {
-//        if (buffMode == buffType.None)
-//        {
-//            base.OnPaint(pe);
-//        }
-//        else
-//        {
-//            System.Drawing.Graphics e = pe.Graphics;
-//            if (this.Image != null)
-//            {
-//                e.DrawImage(this.Image, this.fixedRect);
-//            }
-//
-//            if (0 < count && count <= 99)
-//            {
-//                e.DrawString(count.ToString(), fixedFontCount, Brushes.DarkGreen, fixedPointCount);
-//            }
-//
-//            if (cumulative > 0)
-//            {
-//                if (this.align == CumulativeTextAlign.TopRight)
-//                {
-//
-//                    if (cumulative >= 10)
-//                    {
-//                        e.DrawString(cumulative.ToString(), fixedFontCumulative, Brushes.Black, fixedPointCumulative_2);
-//                    }
-//                    else
-//                    {
-//                        e.DrawString(cumulative.ToString(), fixedFontCumulative, Brushes.Black, fixedPointCumulative);
-//                    }
-//                }
-//                else
-//                {
-//                    e.DrawString(cumulative.ToString(), fixedFontCumulative, Brushes.Black, fixedPointCumulativeCenter);
-//                }
-//
-//            }
-//        }
-//    }
-
-//    public bool AbstractCountDownBuff()
-//    {
-//        if (this.Count > 0)
-//        {
-//            this.Count--;
-//            this.Invalidate();
-//            if (this.Count <= 0)
-//            {
-//                this.Image = null;
-//                this.Update();
-//                return true; // BUFF効果が切れた事を通達する。
-//            }
-//        }
-//        return false;
-//    }
 }
-
