6078023bb448f18896657f5b3becc34ab92a4fa5
verze.artie <verze.artie@gmail.com>
2016-09-08 02:56:42 +0900

    update truthplayback

--- Assets/TruthPlayback.cs
+++ Assets/TruthPlayback.cs
@@ -1,25 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
-
-//using UnityEngine;
-//using UnityEngine.UI;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Net;
-//using System.Net.Sockets;
-//using System;
-//using UnityEngine.EventSystems;
-//using UnityEngine.Events;
 
 namespace DungeonPlayer
 {
     public partial class TruthPlayback : MotherForm
     {
-        public GameObject[] nodeList;
+        public GameObject node;
+        public GameObject content;
 
         public override void Start()
         {
             base.Start();
+
+            RectTransform rect = content.GetComponent<RectTransform>();
+            const int HEIGHT = 110;
+            const int MARGIN = 10;
+            for (int ii = 0; ii < 50; ii++)
+            {
+                GameObject item = GameObject.Instantiate(node);
+                item.transform.SetParent(content.transform, false);
+                item.GetComponentInChildren<Text>().text = "debug " + ii.ToString();
+                item.transform.localPosition = new Vector3(item.transform.localPosition.x, item.transform.localPosition.y - HEIGHT * (ii + 1), item.transform.localPosition.z);
+
+                // 個数に応じて、コンテンツ長さを延長する。
+                rect.sizeDelta = new Vector2(rect.sizeDelta.x, rect.sizeDelta.y + HEIGHT);
+            }
+            // 最後に余白を追加しておく。
+            rect.sizeDelta = new Vector2(rect.sizeDelta.x, rect.sizeDelta.y + MARGIN);
         }
 
         public override void Update()
