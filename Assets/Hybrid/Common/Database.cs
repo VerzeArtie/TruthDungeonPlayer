a3011f0cf160f1dc844f5a732ffc5e89377c3b6b
verze.artie <verze.artie@gmail.com>
2017-05-01 15:40:49 +0900

    adjust camera border

--- Assets/Hybrid/Common/Database.cs
+++ Assets/Hybrid/Common/Database.cs
@@ -1046,10 +1046,10 @@ namespace DungeonPlayer
         public static int TRUTH_FIRST_POS = DUNGEON_MOVE_LEN * 13 + 39; // 後編ダンジョン開始位置
         public static int TRUTH_DUNGEON_COLUMN = 60; // 後編ダンジョンの列数
         public static int TRUTH_DUNGEON_ROW = 40; // 後編ダンジョンの行数
-        public static int CAMERA_BORDER_X_LEFT = 8;
-        public static int CAMERA_BORDER_X_RIGHT = 49;
-        public static int CAMERA_BORDER_Y_TOP = -4;
-        public static int CAMERA_BORDER_Y_BOTTOM = -35;
+        public static int CAMERA_BORDER_X_LEFT = 4;
+        public static int CAMERA_BORDER_X_RIGHT = 52;
+        public static int CAMERA_BORDER_Y_TOP = -3;
+        public static int CAMERA_BORDER_Y_BOTTOM = -36;
         public static int CAMERA_WORLD_POINT_X = 5;
         public static int CAMERA_WORLD_POINT_Y = -3;
 
